4a0aa02ffc1fedf870185a3978adbfc14b9a6b9d
abby-mo <abbymoorefl@gmail.com>
2025-11-05 17:37:08 -0500

    11/5 EditingUserInput
    Working on storing and printing questions

--- src/userInputTest.cpp
+++ src/userInputTest.cpp
@@ -1,104 +1,55 @@
-// #include <vector>
-// #include <string>
-// #include <iostream>
-// #include <sstream>
-// using namespace std;
-// #include "person.h"
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <map>
+using namespace std;
+#include "person.h"
+#include "userInputTest.h"
 
 
-// Person user;
+Person user;
 
-// void personalityTest(){
+questionData dataPoint;
+Questions questionsObj;
+
+void Questions::getValues() {
+    //read from csv file and populate 2d_vector
+    ifstream data("../data/Questions.csv");
+    if(!data.is_open()){
+        cout << "Error opening file" << endl;
+        return;
+    }
+    string headers;
+    getline(data, headers,'\n'); // 1st line
     
-//     float answer; 
-//     string EI;
-//     string NS;
-//     string TF;
-//     string PJ;
-
-//     cout << "We will being with the personality test" << endl;
-
+    string row_str;
     
-//     cout << "Are you more introverted (0.0) or extrover0.ted (1.0)?" << endl;
-//     cin >> answer;
-//     user.setSocialS(answer);
-//     cout << "Great! you are an ";
+    while(getline(data, row_str, '\n')){
+      stringstream ss(row_str);
+      string question, category, flipped_str, first_column, second_column;
     
-//     if(answer > 0.5) {
-//         cout << "introvert" << endl;
-//         EI = "I";
-//     }
-
-//     else {
-//         cout << "extrovert!" << endl;
-//         EI = "E";
-//     }
-
-//     cout << "When you process information, do you think in hard facts (0.0) or abstract concepts (1.0)?";
-//     cin >> answer;
-//     user.setProcessS(answer);
-
-//     cout << "Do you make decisions based on more logic (0.0) or personal values (1.0)?";
-//     cin >> answer;
-//     user.setDecisionS(answer);
-
-//     cout << "Are you more structured (0.0) or flexible (1.0)?";
-//     cin >> answer;
-//     user.setTactics(answer);
-
-
-//     /*Do you make decisions based on more logic (0.0) or personal values (1.0)?
-// Are you more structured (0.0) or flexible (1.0)?
-// Are you below (0.0) or above (1.0) the average height for your gender? (5’9 for men, 5’4 for women)
-// Are your eyes light (0.0) or dark (1.0)?
-// Is your hair light (0.0) or dark (1.0)?
-
-// */
-
-
-
-
-
-
-
-// Questions::getValues() {
-//     //read from csv file and populate 2d_vector
-//     ifstream data("../data/Questions.csv");
-//     if(!data.is_open()){
-//         cout << "Error opening file" << endl;
-//         return 1;
-//     }
-//     string headers;
-//     getline(data, headers,'\n'); // 1st line
-    
-//     string row_str;
-//     //objects initlization
-
-
-    
-//     while(getline(data, row_str, '\n')){
+      getline(ss, question, ',');
+      getline(ss, category, ',');
+      getline(ss, flipped_str, ',');   
+      getline(ss, first_column, ',');
+      getline(ss, second_column, ',');
       
-//       stringstream ss(row_str);
-//         string question, category, flipped_str;
+      dataPoint.question = question;
+      dataPoint.category = category;
+      dataPoint.flipped = true;  // Reset flipped to true for each new question
+      dataPoint.yesIndicates = first_column;
+      dataPoint.noIndicates = second_column;
+      
+      if (first_column == "Extraversion" || first_column == "Intuition" || first_column == "Thinking" || first_column == "Perceiving"){
+        dataPoint.flipped = false;
+      }
+
+      cout << "Question: " << dataPoint.question << ", Category: " << dataPoint.category << ", Flipped?: " << dataPoint.flipped << endl;
+      
+    }
     
-//       getline(ss, question, ',');
-//       getline(ss, category, ',');
-//       getline(ss, flipped_str, ',');   
-//       getline(ss, first_column, ',');
-      
-      
-//       if (first_column == "Social" || first_column == "Processing" || first_column == "Descision" || first_column == "Tactics"){
-//         flipped == T;
-//       }
 
+}
 
-      
-//     }
-// }
-
-    
-// }
-
-// void physicalTest(){
-//     cout << "We will now move on to the physical test" << endl;
-// }
--- src/userInputTest.h
+++ src/userInputTest.h
@@ -1,39 +1,42 @@
-//     /*    needs to store: 
-//     - a string for the question answer and more
-//     public:
-//         ask_question(){
-//             cout<< the question
-//         }
-// */
-// #include <string>
-// #include <vector>
-// using namespace std;
+    /*    needs to store: 
+    - a string for the question answer and more
+    public:
+        ask_question(){
+            cout<< the question
+        }
+*/
+#include <string>
+#include <vector>
+using namespace std;
 
 
-// struct questionData
-// {
-//     string question;
-//     string category;
-//     bool flipped;
+struct questionData
+{
+    string question;
+    string category;
+    bool flipped = true;
+    string yesIndicates;
+    string noIndicates;
     
-//     int answer;
+    int answer;
 
-//     /* data */
-// };
+    /* data */
+};
 
-// class Questions{
-//     public: 
-//         vector<vector<questionData>> 2d_vector;
+class Questions{
+    public: 
+        void ask_question();
+        void getValues();
 
-//         void ask_question();
-
-//         void getValues();
+    private:
+        vector<vector<questionData>> matrix_vector;
     
-// }
+};
 
-// 2d_vector entry1 = {"Meow meow", 7}
 
-// //test case so u cant take in an int
 
-// //a question and what a yes/no implies for each quesion
- 
+
+
+//test case so u cant take in an int
+
+//a question and what a yes/no implies for each quesion
